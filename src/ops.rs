be9154395dedbb7032df6866941f68e6a76f4c1e
Natalya McKay <zea_64@proton.me>
2024-02-24 18:13:17 -0500

    Add all `openat` arguments and clean up formatting.

--- src/ops.rs
+++ src/ops.rs
@@ -1,5 +1,6 @@
 use core::{
 	cell::RefCell,
+	ffi::CStr,
 	future::Future,
 	mem::MaybeUninit,
 	num::NonZeroU64,
@@ -7,21 +8,19 @@ use core::{
 	task::{Context, Poll},
 };
 
-use std::ffi::CString;
-
 use rustix::{
 	fd::{AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, OwnedFd, RawFd},
-	fs::{self, Mode, OFlags},
-	io::{self, Errno},
+	fs::{Mode, OFlags},
+	io::Errno,
 	io_uring::*,
 };
 
 use crate::*;
 
 macro_rules! zero {
-    () => {
+	() => {
 		MaybeUninit::zeroed().assume_init()
-    };
+	};
 }
 
 fn res_or_errno(result: i32) -> Result<i32, Errno> {
@@ -38,10 +37,7 @@ pub struct Op<'a> {
 
 impl<'a> Op<'a> {
 	pub fn new(ring: &'a RefCell<Uring>) -> Self {
-		Op {
-			ring,
-			ticket: None,
-		}
+		Op { ring, ticket: None }
 	}
 }
 
@@ -69,7 +65,8 @@ impl<'a> Op<'a> {
 		let ticket = ring.get_ticket();
 		self.ticket = Some(ticket);
 
-		ring.push(sqe_factory().set_user_data(ticket.into())).unwrap();
+		ring.push(sqe_factory().set_user_data(ticket.into()))
+			.unwrap();
 
 		cx.waker().wake_by_ref();
 
@@ -85,7 +82,10 @@ impl<'a> Future for Nop<'a> {
 	type Output = ();
 
 	fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-		Pin::into_inner(self).op.poll(cx, || unsafe{Sqe::new(zero!())}).map(|_cqe| ())
+		Pin::into_inner(self)
+			.op
+			.poll(cx, || unsafe { Sqe::new(zero!()) })
+			.map(|_cqe| ())
 	}
 }
 
@@ -98,7 +98,10 @@ impl<'a> Nop<'a> {
 // TODO: Other options.
 pub struct Openat<'a> {
 	op: Op<'a>,
-	path: CString,
+	dfd: BorrowedFd<'a>,
+	path: &'a CStr,
+	flags: OFlags,
+	mode: Mode,
 }
 
 impl<'a> Future for Openat<'a> {
@@ -107,16 +110,50 @@ impl<'a> Future for Openat<'a> {
 	fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
 		let this = Pin::into_inner(self);
 		let sqe_factory = || unsafe {
-			Sqe::new(io_uring_sqe { opcode: IoringOp::Openat, flags: IoringSqeFlags::empty(), ioprio: zero!(), fd: rustix::fs::CWD.as_raw_fd(), off_or_addr2: zero!(), addr_or_splice_off_in: addr_or_splice_off_in_union { addr: io_uring_ptr::from(this.path.as_ptr() as *mut c_void) }, len: zero!(), op_flags: zero!(), user_data: zero!(), buf: zero!(), personality: zero!() , splice_fd_in_or_file_index: zero!(), addr3_or_cmd: zero!() })
+			Sqe::new(io_uring_sqe {
+				opcode: IoringOp::Openat,
+				flags: IoringSqeFlags::empty(),
+				ioprio: zero!(),
+				fd: this.dfd.as_raw_fd(),
+				off_or_addr2: zero!(),
+				addr_or_splice_off_in: addr_or_splice_off_in_union {
+					addr: io_uring_ptr::from(this.path.as_ptr() as *mut c_void),
+				},
+				len: len_union {
+					len: this.mode.bits(),
+				},
+				op_flags: op_flags_union {
+					open_flags: this.flags,
+				},
+				user_data: zero!(),
+				buf: zero!(),
+				personality: zero!(),
+				splice_fd_in_or_file_index: zero!(),
+				addr3_or_cmd: zero!(),
+			})
 		};
 
-		this.op.poll(cx, sqe_factory).map(|cqe| unsafe { OwnedFd::from_raw_fd(cqe.res) })
+		this.op
+			.poll(cx, sqe_factory)
+			.map(|cqe| unsafe { OwnedFd::from_raw_fd(cqe.res) })
 	}
 }
 
 impl<'a> Openat<'a> {
-	pub fn new(ring: &'a RefCell<Uring>, path: CString) -> Self {
-		Openat { op: Op::new(ring), path }
+	pub fn new(
+		ring: &'a RefCell<Uring>,
+		dfd: BorrowedFd<'a>,
+		path: &'a CStr,
+		flags: OFlags,
+		mode: Mode,
+	) -> Self {
+		Openat {
+			op: Op::new(ring),
+			dfd,
+			path,
+			flags,
+			mode,
+		}
 	}
 }
 
@@ -132,16 +169,40 @@ impl<'a> Future for Read<'a> {
 	fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
 		let this = Pin::into_inner(self);
 		let sqe_factory = || unsafe {
-			Sqe::new(io_uring_sqe { opcode: IoringOp::Read, flags: IoringSqeFlags::empty(), ioprio: zero!(), fd: this.fd.as_raw_fd(), off_or_addr2: off_or_addr2_union { off: u64::MAX }, addr_or_splice_off_in: addr_or_splice_off_in_union { addr: io_uring_ptr::from(this.buf.as_mut_ptr().cast()) }, len: len_union { len: this.buf.len() as u32}, op_flags: zero!(), user_data: zero!(), buf: zero!(), personality: zero!(), splice_fd_in_or_file_index: zero!(), addr3_or_cmd: zero!() })
+			Sqe::new(io_uring_sqe {
+				opcode: IoringOp::Read,
+				flags: IoringSqeFlags::empty(),
+				ioprio: zero!(),
+				fd: this.fd.as_raw_fd(),
+				off_or_addr2: off_or_addr2_union { off: u64::MAX },
+				addr_or_splice_off_in: addr_or_splice_off_in_union {
+					addr: io_uring_ptr::from(this.buf.as_mut_ptr().cast()),
+				},
+				len: len_union {
+					len: this.buf.len() as u32,
+				},
+				op_flags: zero!(),
+				user_data: zero!(),
+				buf: zero!(),
+				personality: zero!(),
+				splice_fd_in_or_file_index: zero!(),
+				addr3_or_cmd: zero!(),
+			})
 		};
 
-		this.op.poll(cx, sqe_factory).map(|cqe| res_or_errno(cqe.res))
+		this.op
+			.poll(cx, sqe_factory)
+			.map(|cqe| res_or_errno(cqe.res))
 	}
 }
 
 impl<'a> Read<'a> {
 	pub fn new(ring: &'a RefCell<Uring>, fd: BorrowedFd<'a>, buf: &'a mut [u8]) -> Self {
-		Read { op: Op::new(ring), buf, fd }
+		Read {
+			op: Op::new(ring),
+			buf,
+			fd,
+		}
 	}
 }
 
@@ -156,16 +217,35 @@ impl<'a> Future for Close<'a> {
 	fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
 		let this = Pin::into_inner(self);
 		let sqe_factory = || unsafe {
-			Sqe::new(io_uring_sqe { opcode: IoringOp::Close, flags: IoringSqeFlags::empty(), ioprio: zero!() , fd: this.fd, off_or_addr2: zero!(), addr_or_splice_off_in: zero!(), len: zero!(), op_flags: zero!(), user_data: zero!(), buf: zero!(), personality: zero!(), splice_fd_in_or_file_index: zero!(), addr3_or_cmd: zero!() })
+			Sqe::new(io_uring_sqe {
+				opcode: IoringOp::Close,
+				flags: IoringSqeFlags::empty(),
+				ioprio: zero!(),
+				fd: this.fd,
+				off_or_addr2: zero!(),
+				addr_or_splice_off_in: zero!(),
+				len: zero!(),
+				op_flags: zero!(),
+				user_data: zero!(),
+				buf: zero!(),
+				personality: zero!(),
+				splice_fd_in_or_file_index: zero!(),
+				addr3_or_cmd: zero!(),
+			})
 		};
 
-		this.op.poll(cx, sqe_factory).map(|cqe| res_or_errno(cqe.res).map(|_res| ()))
+		this.op
+			.poll(cx, sqe_factory)
+			.map(|cqe| res_or_errno(cqe.res).map(|_res| ()))
 	}
 }
 
 impl<'a> Close<'a> {
 	pub fn new(ring: &'a RefCell<Uring>, fd: OwnedFd) -> Self {
-		Close { op: Op::new(ring), fd: fd.into_raw_fd() }
+		Close {
+			op: Op::new(ring),
+			fd: fd.into_raw_fd(),
+		}
 	}
 }
 
@@ -174,7 +254,7 @@ mod test {
 	use core::{cell::RefCell, future::join};
 
 	use futures::executor::block_on;
-use rustix::fd::AsFd;
+	use rustix::{fd::AsFd, fs};
 
 	use crate::ops::*;
 
@@ -231,7 +311,13 @@ use rustix::fd::AsFd;
 	fn openat() {
 		let ring = RefCell::new(Uring::new().unwrap());
 
-		let fd = block_on(Openat::new(&ring, CString::new("/dev/zero").unwrap()));
+		let fd = block_on(Openat::new(
+			&ring,
+			rustix::fs::CWD,
+			CStr::from_bytes_with_nul(b"/dev/zero\0").unwrap(),
+			OFlags::RDONLY,
+			Mode::empty(),
+		));
 
 		let mut buf = [1u8; 64];
 
