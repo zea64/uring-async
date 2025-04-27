cdea0a8d9c567f84dac94ac6abbab0e960047c3c
Natalya McKay <zea_64@proton.me>
2025-04-27 18:19:49 -0400

    Redo everything again, starting with `Uring`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,397 +1,247 @@
-#![feature(future_join)]
-
-pub mod ops;
-pub mod sync;
-
 use core::{
-	cell::RefCell,
+	error::Error,
 	ffi::c_void,
-	fmt::{self, Debug},
-	marker::PhantomPinned,
-	mem::size_of,
-	ops::{Deref, DerefMut, Rem},
-	pin::Pin,
+	fmt,
+	mem::{self, MaybeUninit},
 	ptr::{self, NonNull},
-	task::{Context, Poll, Waker},
+	slice,
 };
+use std::os::fd::AsFd;
 
 use rustix::{
 	fd::OwnedFd,
-	io::{Errno, Result as PosixResult},
+	io::Errno,
 	io_uring::*,
 	mm::{MapFlags, ProtFlags, mmap, munmap},
 };
 
-fn posix_result(ret: i32) -> PosixResult<u32> {
-	match ret {
-		x if x < 0 => Err(Errno::from_raw_os_error(-x)),
-		x => Ok(x as u32),
+#[derive(Debug)]
+struct Mmapped(NonNull<c_void>, usize);
+
+impl Mmapped {
+	unsafe fn new(ptr: *mut c_void, size: usize) -> Self {
+		Self(NonNull::new(ptr).unwrap(), size)
+	}
+
+	fn as_ptr(&self) -> *mut c_void {
+		self.0.as_ptr()
 	}
 }
 
-struct Queue<T: 'static + Debug> {
-	our_ptr: &'static mut u32,
-	their_ptr: &'static mut u32,
-	flags: &'static mut u32,
-	array: &'static mut [T],
-}
-
-impl<T: Debug> Debug for Queue<T> {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		f.debug_struct("Queue")
-			.field("our_ptr", self.our_ptr)
-			.field("their_ptr", self.their_ptr)
-			.field("flags", self.flags)
-			.field("array addr", &self.array.as_ptr())
-			.field("array len", &self.array.len())
-			.finish()
+impl Drop for Mmapped {
+	fn drop(&mut self) {
+		let _ = unsafe { munmap(self.0.as_ptr(), self.1) };
 	}
 }
 
-impl<T: Default + Debug> Queue<T> {
-	fn mask(&self) -> u32 {
-		self.len() - 1
-	}
+#[derive(Debug)]
+struct RingBuffer<'a, T> {
+	ring: &'a mut [MaybeUninit<T>],
+	head: &'a mut u32,
+	tail: &'a mut u32,
+}
 
-	fn len(&self) -> u32 {
-		self.array.len() as u32
-	}
-
-	fn get_empty_nth(&mut self, seek: i32) -> Option<&mut T> {
-		let effective_our = (*self.our_ptr as i32).wrapping_add(seek) as u32;
-		let their = *self.their_ptr;
-
-		// Queue is full.
-		if effective_our.wrapping_sub(their) & self.mask() == 0 && effective_our != their {
-			None
-		} else {
-			Some(unsafe {
-				self.array
-					.get_unchecked_mut((effective_our & self.mask()) as usize)
-			})
+impl<T> RingBuffer<'_, T> {
+	fn push(&mut self, input: T) -> Result<(), T> {
+		if self.len() == self.capacity() {
+			return Err(input);
 		}
-	}
 
-	fn push(&mut self, item: &mut T) -> Result<(), ()> {
-		match self.get_empty_nth(0) {
-			Some(slot) => {
-				*slot = core::mem::take(item);
-				*self.our_ptr += 1;
-				Ok(())
-			}
-			None => Err(()),
-		}
+		self.index_mut(*self.tail).write(input);
+		*self.tail += 1;
+		Ok(())
 	}
 
 	fn pop(&mut self) -> Option<T> {
-		if *self.our_ptr == *self.their_ptr {
+		if self.len() == 0 {
 			return None;
 		}
 
-		let item = unsafe {
-			self.array
-				.get_unchecked((*self.our_ptr).rem(self.array.len() as u32) as usize)
-		};
-		// Effectively a `.take()`, but since we're never looking at that slot again there's no need to clear it.
-		let result = unsafe { ptr::read(item) };
+		let value = unsafe { self.index_mut(*self.head).assume_init_read() };
+		*self.head += 1;
+		Some(value)
+	}
 
-		*self.our_ptr += 1;
+	#[inline]
+	fn len(&self) -> u32 {
+		let diff = *self.tail - *self.head;
+		assert!(diff <= self.capacity());
+		diff
+	}
 
-		Some(result)
+	#[inline]
+	fn capacity(&self) -> u32 {
+		self.ring.len() as u32
+	}
+
+	#[inline]
+	fn index_mut(&mut self, idx: u32) -> &mut MaybeUninit<T> {
+		let idx = idx & (self.capacity() - 1);
+		unsafe { self.ring.get_unchecked_mut(idx as usize) }
+	}
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct QueueFullError;
+
+impl Error for QueueFullError {}
+
+impl fmt::Display for QueueFullError {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		f.write_str("io_uring submission queue full")
 	}
 }
 
 #[derive(Debug)]
 pub struct Uring {
+	cq_mmap: Mmapped,
+	sq_mmap: Mmapped,
 	fd: OwnedFd,
-	sq: Queue<Sqe>,
-	cq: Queue<Cqe>,
-	queue_map_len: u32,
-	queue_base: NonNull<u32>,
-	sq_entries: NonNull<Sqe>,
-	in_flight: u32,
+	cq_head_off: u32,
+	cq_tail_off: u32,
+	cq_cqes_off: u32,
+	cq_size: u32,
+	sq_head_off: u32,
+	sq_tail_off: u32,
+	sq_size: u32,
 }
 
 impl Uring {
-	const NR_SQ_ENTRIES: u32 = 4096;
+	fn cq(&mut self) -> RingBuffer<'_, io_uring_cqe> {
+		unsafe {
+			let base: *mut u32 = self.cq_mmap.as_ptr().cast();
+			let ring = slice::from_raw_parts_mut(
+				base.byte_offset(self.cq_cqes_off.try_into().unwrap())
+					.cast(),
+				self.cq_size.try_into().unwrap(),
+			);
+			let head = &mut *base.byte_offset(self.cq_head_off.try_into().unwrap());
+			let tail = &mut *base.byte_offset(self.cq_tail_off.try_into().unwrap());
+			RingBuffer { ring, head, tail }
+		}
+	}
 
-	pub fn new() -> PosixResult<Self> {
-		let mut params = io_uring_params {
-			sq_entries: Uring::NR_SQ_ENTRIES,
-			cq_entries: Uring::NR_SQ_ENTRIES,
-			flags: IoringSetupFlags::SUBMIT_ALL
-				| IoringSetupFlags::COOP_TASKRUN
-				| IoringSetupFlags::SINGLE_ISSUER
-				| IoringSetupFlags::DEFER_TASKRUN,
-			sq_thread_cpu: 0,
-			sq_thread_idle: 0,
-			features: IoringFeatureFlags::empty(),
-			wq_fd: 0,
-			resv: [0; 3],
-			sq_off: Default::default(),
-			cq_off: Default::default(),
-		};
+	fn sq(&mut self) -> RingBuffer<'_, io_uring_sqe> {
+		unsafe {
+			let ring = slice::from_raw_parts_mut(
+				self.sq_mmap.as_ptr().cast(),
+				self.sq_size.try_into().unwrap(),
+			);
 
-		let fd = io_uring_setup(Uring::NR_SQ_ENTRIES, &mut params)?;
+			let base: *mut u32 = self.cq_mmap.as_ptr().cast();
+			let head = &mut *base.byte_offset(self.sq_head_off.try_into().unwrap());
+			let tail = &mut *base.byte_offset(self.sq_tail_off.try_into().unwrap());
+			RingBuffer { ring, head, tail }
+		}
+	}
 
-		// Make sure this kernel supports all the features we assume.
+	pub fn new(entries: u32) -> Result<Self, Errno> {
+		let mut params = io_uring_params::default();
+		params.flags = IoringSetupFlags::CLAMP
+			| IoringSetupFlags::SUBMIT_ALL
+			| IoringSetupFlags::COOP_TASKRUN
+			| IoringSetupFlags::SINGLE_ISSUER
+			| IoringSetupFlags::DEFER_TASKRUN
+			| IoringSetupFlags::NO_SQARRAY;
+
+		let fd = unsafe { io_uring_setup(entries.next_power_of_two(), &mut params) }?;
 		if !params.features.contains(
 			IoringFeatureFlags::SINGLE_MMAP
 				| IoringFeatureFlags::NODROP
-				| IoringFeatureFlags::SUBMIT_STABLE,
+				| IoringFeatureFlags::LINKED_FILE,
 		) {
-			return Err(rustix::io::Errno::NOSYS);
+			return Err(Errno::NOSYS);
 		}
 
-		unsafe {
-			let queue_map_len =
-				params.sq_off.array as usize + params.sq_entries as usize * size_of::<u32>();
-			let queues_ptr = mmap(
-				ptr::null_mut(),
-				queue_map_len,
-				ProtFlags::READ | ProtFlags::WRITE,
-				MapFlags::SHARED | MapFlags::POPULATE,
-				&fd,
-				IORING_OFF_SQ_RING,
-			)?;
-			let sq_entries_ptr = mmap(
-				ptr::null_mut(),
-				params.sq_entries as usize * size_of::<io_uring_sqe>(),
-				ProtFlags::READ | ProtFlags::WRITE,
-				MapFlags::SHARED | MapFlags::POPULATE,
-				&fd,
-				IORING_OFF_SQES,
-			)?;
+		let cq_size = params.cq_off.cqes as usize
+			+ params.cq_entries as usize * mem::size_of::<io_uring_cqe>();
+		let sq_size = params.sq_entries as usize * mem::size_of::<io_uring_sqe>();
 
-			// Map sq array position `i` to index `i` in entries array so we can just forget about it.
-			let sq_array: *mut u32 = queues_ptr.byte_offset(params.sq_off.array as isize).cast();
-			for i in 0..params.sq_entries {
-				*sq_array.offset(i as isize) = i;
-			}
-
-			#[inline]
-			unsafe fn to_ptr(base: *mut c_void, offset: u32) -> &'static mut u32 {
-				unsafe { &mut *base.byte_offset(offset as isize).cast() }
-			}
-
-			let sq: Queue<Sqe> = Queue {
-				our_ptr: to_ptr(queues_ptr, params.sq_off.tail),
-				their_ptr: to_ptr(queues_ptr, params.sq_off.head),
-				flags: to_ptr(queues_ptr, params.sq_off.flags),
-				array: core::slice::from_raw_parts_mut(
-					sq_entries_ptr.cast(),
-					params.sq_entries as usize,
-				),
-			};
-
-			let cq: Queue<Cqe> = Queue {
-				our_ptr: to_ptr(queues_ptr, params.cq_off.head),
-				their_ptr: to_ptr(queues_ptr, params.cq_off.tail),
-				flags: to_ptr(queues_ptr, params.cq_off.flags),
-				array: core::slice::from_raw_parts_mut(
-					queues_ptr.byte_offset(params.cq_off.cqes as isize).cast(),
-					params.cq_entries as usize,
-				),
-			};
-
-			Ok(Self {
-				fd,
-				sq,
-				cq,
-				sq_entries: NonNull::new_unchecked(sq_entries_ptr.cast()),
-				queue_base: NonNull::new_unchecked(queues_ptr.cast()),
-				queue_map_len: queue_map_len as u32,
-				in_flight: 0,
-			})
-		}
-	}
-
-	pub fn push(&mut self, mut sqe: Sqe) -> Option<()> {
-		loop {
-			let x = self.sq.push(&mut sqe);
-
-			if x.is_ok() {
-				return Some(());
-			}
-
-			self.submit(0)?;
-		}
-	}
-
-	pub fn submit(&mut self, min_completions: u32) -> Option<u32> {
-		let to_submit = *self.sq.our_ptr - *self.sq.their_ptr;
-		self.in_flight += to_submit;
-
-		if min_completions > self.in_flight {
-			return None;
-		}
-
-		let submitted = loop {
-			match unsafe {
-				io_uring_enter(
-					&self.fd,
-					to_submit,
-					min_completions,
-					IoringEnterFlags::GETEVENTS,
-					ptr::null(),
-					0,
-				)
-			} {
-				Ok(x) => break x,
-				Err(Errno::AGAIN | Errno::INTR) => continue,
-				x => x.expect("io_uring_enter failed"),
-			};
+		let cq_mmap = unsafe {
+			Mmapped::new(
+				mmap(
+					ptr::null_mut(),
+					cq_size,
+					ProtFlags::READ | ProtFlags::WRITE,
+					MapFlags::POPULATE | MapFlags::SHARED_VALIDATE,
+					fd.as_fd(),
+					IORING_OFF_SQ_RING,
+				)?,
+				cq_size,
+			)
 		};
 
-		while let Some(cqe) = self.cq.pop() {
-			self.in_flight -= 1;
-
-			let pending_cqe = unsafe { &mut *cqe.user_data.ptr().cast::<PendingCqe>() };
-
-			if let PendingCqe::Pending(waker, _) = pending_cqe {
-				waker.wake_by_ref();
-				*pending_cqe = PendingCqe::Complete(cqe);
-			} else {
-				unreachable!();
-			}
-		}
-
-		Some(submitted)
-	}
-
-	pub fn sq_enqueued(&self) -> u16 {
-		(*self.sq.our_ptr - *self.sq.their_ptr).try_into().unwrap()
-	}
-
-	pub fn cq_enqueued(&self) -> u16 {
-		(*self.cq.their_ptr - *self.cq.our_ptr).try_into().unwrap()
-	}
-
-	pub fn in_flight(&self) -> u32 {
-		self.in_flight
-	}
-}
-
-impl Drop for Uring {
-	fn drop(&mut self) {
-		unsafe {
-			let nr_sq_entries = self.sq.len() as usize;
-
-			munmap(self.queue_base.as_ptr().cast(), self.queue_map_len as usize).unwrap();
-
-			munmap(
-				self.sq_entries.as_ptr().cast(),
-				nr_sq_entries * size_of::<io_uring_sqe>(),
+		let sq_mmap = unsafe {
+			Mmapped::new(
+				mmap(
+					ptr::null_mut(),
+					sq_size,
+					ProtFlags::READ | ProtFlags::WRITE,
+					MapFlags::POPULATE | MapFlags::SHARED_VALIDATE,
+					fd.as_fd(),
+					IORING_OFF_SQES,
+				)?,
+				sq_size,
 			)
-			.unwrap();
-		}
+		};
+
+		assert!(params.cq_entries.is_power_of_two());
+		assert!(params.sq_entries.is_power_of_two());
+
+		Ok(Uring {
+			fd,
+			cq_mmap,
+			sq_mmap,
+			cq_head_off: params.cq_off.head,
+			cq_tail_off: params.cq_off.tail,
+			cq_cqes_off: params.cq_off.cqes,
+			cq_size: params.cq_entries,
+			sq_head_off: params.sq_off.head,
+			sq_tail_off: params.sq_off.tail,
+			sq_size: params.sq_entries,
+		})
 	}
-}
 
-#[derive(Default)]
-#[repr(transparent)]
-pub struct Sqe(pub io_uring_sqe);
-
-impl Debug for Sqe {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		f.debug_struct("Sqe")
-			.field("user_data", &self.user_data.ptr())
-			.finish()
+	/// # Safety
+	/// `sqe` sends low level commands to the kernel that, for instance, can read and write to arbitrary memory. Consult the safety conditions of the underlying io_uring operations and corresponding syscalls.
+	pub unsafe fn push(&mut self, sqe: io_uring_sqe) -> Result<(), QueueFullError> {
+		self.sq().push(sqe).map_err(|_| QueueFullError)
 	}
-}
 
-impl Deref for Sqe {
-	type Target = io_uring_sqe;
-
-	fn deref(&self) -> &Self::Target {
-		&self.0
-	}
-}
-
-impl DerefMut for Sqe {
-	fn deref_mut(&mut self) -> &mut Self::Target {
-		&mut self.0
-	}
-}
-
-#[repr(transparent)]
-#[derive(Debug, Default)]
-pub struct Cqe(pub io_uring_cqe);
-
-impl Deref for Cqe {
-	type Target = io_uring_cqe;
-
-	fn deref(&self) -> &Self::Target {
-		&self.0
-	}
-}
-
-impl DerefMut for Cqe {
-	fn deref_mut(&mut self) -> &mut Self::Target {
-		&mut self.0
-	}
-}
-
-impl Clone for Cqe {
-	fn clone(&self) -> Self {
-		Self(unsafe { ptr::read(&self.0) })
-	}
-}
-
-#[derive(Debug)]
-enum PendingCqe {
-	Pending(Waker, PhantomPinned),
-	Complete(Cqe),
-}
-
-impl Default for PendingCqe {
-	fn default() -> Self {
-		Self::Complete(Cqe::default())
-	}
-}
-
-pub fn block_on<F: Future>(ring: &RefCell<Uring>, mut fut: F) -> F::Output {
-	loop {
-		if let Poll::Ready(x) = Future::poll(
-			unsafe { Pin::new_unchecked(&mut fut) },
-			&mut Context::from_waker(Waker::noop()),
-		) {
-			break x;
-		}
-
-		let mut borrowed_ring = ring.borrow_mut();
-		if borrowed_ring.sq_enqueued() != 0 || borrowed_ring.in_flight != 0 {
-			borrowed_ring.submit(1);
-		}
+	pub fn pop(&mut self) -> Option<io_uring_cqe> {
+		self.cq().pop()
 	}
 }
 
 #[cfg(test)]
 mod test {
-	use core::{cell::RefCell, future::join};
-
 	use crate::*;
 
 	#[test]
-	fn new_ring() {
-		let _ = Uring::new().unwrap();
-	}
+	fn basic() {
+		let mut ring = Uring::new(64).unwrap();
+		let mut sq = ring.sq();
 
-	#[test]
-	fn nop() {
-		let ring = RefCell::new(Uring::new().unwrap());
+		for i in 0.. {
+			let mut nop: io_uring_sqe = unsafe { MaybeUninit::zeroed().assume_init() };
+			nop.opcode = IoringOp::Nop;
+			nop.user_data.u64_ = i;
 
-		block_on(&ring, async {
-			ops::Nop::new(&ring).await;
-		})
-	}
+			if sq.push(nop).is_err() {
+				break;
+			}
+		}
 
-	#[test]
-	fn many_nop() {
-		let ring = RefCell::new(Uring::new().unwrap());
+		let len = ring.sq().len();
+		let res = unsafe { io_uring_enter(ring.fd.as_fd(), len, len, IoringEnterFlags::GETEVENTS) }
+			.unwrap();
+		assert_eq!(res, 64);
 
-		block_on(&ring, async {
-			join!(ops::Nop::new(&ring), ops::Nop::new(&ring)).await;
-		})
+		let mut cq = ring.cq();
+		for i in 0..64 {
+			let cqe = cq.pop().unwrap();
+			assert_eq!(cqe.user_data.u64_(), i);
+		}
+		assert!(cq.pop().is_none());
 	}
 }
